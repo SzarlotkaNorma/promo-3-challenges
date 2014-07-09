cb34b29bc4d02ec6b90174f106837dd826c91229
SzarlotkaNorma <charlottenorlund@gmail.com>
2014-07-09 15:23:48 +0200

    sorting the wagon narly done missing the AND

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -7,8 +7,19 @@ begin
   name = gets
   name.chomp! if name
 
+  students << name if name != ""
+
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+	#call wagon_sort by creating an array
+	#insert "and" before last name -2
+
+students = wagon_sort(students)
+
+
+
+puts "Congratulations! Your Wagon has #{students.length} students:"
+puts "- #{students.join(", ")}"
