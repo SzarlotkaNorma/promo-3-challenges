68c3095ed20f653de9837e37c58c7873a4e8a241
SzarlotkaNorma <charlottenorlund@gmail.com>
2014-07-11 11:56:27 +0200

    poor calories counter done

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,5 +1,20 @@
 def poor_calories_counter(burger, side, beverage)
   #TODO: return number of calories for this mcDonald order
+
+  sum = 0
+  calories = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170,
+  }
+
+  calories[burger] + calories[side] + calories[beverage]
+
 end
 
 def calories_counter(*orders)
