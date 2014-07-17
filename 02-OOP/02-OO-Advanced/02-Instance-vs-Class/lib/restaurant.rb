402ac7a562e49be2431e820b584df9f1ef94042d
SzarlotkaNorma <charlottenorlund@gmail.com>
2014-07-17 14:51:29 +0200

    exercice started

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,23 @@
 class Restaurant
-  
+
   #TODO add relevant accessors if necessary
-  
-  def initialize(city, name)
+  attr_accessor :average_rating
+
+  def initialize(city, name, average_rating)
     #TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @average_rating = average_rating
   end
-  
+
+  def rate(average_rating)
+  end
+
   #TODO: implement #filter_by_city and #rate methods
-  
+
+  def self.filter_by_city(retaurants, city)
+
+  end
+
 end
+
