846a9f335eeebe38f2fba5cb45ae68075d5eea50
SzarlotkaNorma <charlottenorlund@gmail.com>
2014-07-17 16:00:13 +0200

    nah

--- 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
@@ -4,24 +4,24 @@ class Animal
     @name = name
     #TODO: find good instruction
   end
-  
+
   def specie(specie)
     @specie = specie
     #TODO: find good instruction
   end
-  
+
   def color(color)
     @color = color
     #TODO: find good instruction
   end
-  
+
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
     #TODO: find good instruction
   end
-  
+
   def to_s
-    "Name: #{@name}, Specie: #{@specie}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
+    "self.Name: #{@name}, Specie: #{@specie}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
-  
+
 end
