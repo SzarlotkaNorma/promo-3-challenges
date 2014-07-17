dfeac1319cba5f9459465aa01fe794921ba2f034
SzarlotkaNorma <charlottenorlund@gmail.com>
2014-07-17 11:11:55 +0200

    blocked by super

--- 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
+++ 02-OOP/02-OO-Advanced/01-Dessert-inheritance/lib/dessert.rb
@@ -1,15 +1,35 @@
 class Dessert
+  attr_accessor :name, :calories
+
   def initialize(name, calories)
-  end 
-  
-  def healthy?
+    @name = name
+    @calories = calories
   end
-  
+
+  def healthy?
+    if calories < 200
+      true
+    else false
+    end
+  end
+
   def delicious?
+    true
   end
 end
 
-class JellyBean < Dessert  
+class JellyBean < Dessert
+  attr_accessor :flavor
+
   def initialize(name, calories, flavor)
+    @name = name
+    @calories = calories
+    @flavor = flavor
+  end
+
+  def delicious?
+    super unless flavor == "black licorice"
   end
 end
+
+
