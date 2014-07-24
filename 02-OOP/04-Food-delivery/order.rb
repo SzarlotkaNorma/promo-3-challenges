556f0beb2937d0e0abfef38f05108227b83b6492
SzarlotkaNorma <charlottenorlund@gmail.com>
2014-07-24 10:58:04 +0200

    food delivery exercice done with Nordine and Juliette

--- 02-OOP/04-Food-delivery/order.rb
+++ 02-OOP/04-Food-delivery/order.rb
@@ -1,17 +1,15 @@
 class Order
 
 attr_reader :order_id, :date
-attr_accessor :content, :customer_id, :employee_id, :completed?
+attr_accessor :order_content, :customer_id, :employee_id, :completed?
 
-  def initialize(order_id, date)
-    @order_id = order_id
+  def initialize(id, date)
+    @id = id
     @date = Time.new
 
     @order_content = []
-    @customer_id = customer_id
-    @employee_id = employee_id
+    @customer_id = 0
+    @employee_id  = 0
 
     @completed? = false
-  end
-
-end
+  en
