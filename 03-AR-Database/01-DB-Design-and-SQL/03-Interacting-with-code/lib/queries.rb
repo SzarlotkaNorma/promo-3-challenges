2d5015c98faedd08942d16fdefc627fc14bea3a1
SzarlotkaNorma <charlottenorlund@gmail.com>
2014-07-28 11:04:51 +0200

    sql exercices

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -1,21 +1,38 @@
 require 'sqlite3'
 
-# opens the database
+#locates the database file
 database_path = File.join(File.dirname(__FILE__), 'db/jukebox.sqlite')
+
+#opens a connection to database
 db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
   #TODO: count number of rows in table table_name
+  sql_query = "SELECT COUNT(*) FROM #{table_name}"
+  rows = db.execute(sql_query)
+  first_line = rows[0]
+  first_column = first_line[0]
+  return first_column
 end
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
+  sql_query = "SELECT Name FROM Artist ORDER BY Name ASC"
+  rows = db.execute(sql_query)
+
+  rows.flatten
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
+  sql_query = "SELECT Name FROM Track WHERE Name LIKE '%love%'"
+  rows = db.execute(sql_query)
+  rows.flatten
 end
 
 def long_tracks(db, min_length)
   #TODO: return tracks verifying: duration > min_length (minutes)
+  sql_query = "SELECT Name FROM Track WHERE Milliseconds > #{min_length * 60 * 1000}"
+  rows = db.execute(sql_query)
+  rows.flatten
 end
--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -6,6 +6,15 @@ db = SQLite3::Database.new(DATABASE_PATH)
 
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist
+  sql_query = "
+    SELECT Track.Name, Album.Title, Artist.Name
+    FROM Track
+    JOIN Album ON Title.AlbumId = Album.AlbumId
+    JOIN Artist ON Album.ArtistId = Artist.ArtistId
+  "
+  rows = db.execute(sql_query)
+
+  rows
 end
 
 def stats_on(db, category)
