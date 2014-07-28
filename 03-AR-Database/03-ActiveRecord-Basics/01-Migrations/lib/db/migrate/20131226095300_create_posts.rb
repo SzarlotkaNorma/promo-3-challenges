b8676837eb93a46a72db87f0589a22c00757ce93
SzarlotkaNorma <charlottenorlund@gmail.com>
2014-07-28 12:44:25 +0200

    migrate done

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/lib/db/migrate/20131226095300_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/lib/db/migrate/20131226095300_create_posts.rb
@@ -1,5 +1,15 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     # TODO: your code here to create the posts table
+
+    #name:string
+    #source_url:string
+    #created_at:datetime
+
+    create_table :posts do |t|
+      t.string    :name
+      t.string    :source_url
+      t.datetime  :created_at
+    end
   end
 end
